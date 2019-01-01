ca2ef9d3f80137afc60c221afe1dc51bc8090f05
Tatiana Androsova <androsovatati@mail.ru>
2019-01-01 23:23:20 +0400

    Handle user login

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,6 +3,7 @@ import ScreenWrapper from "../../components/ScreenWrapper";
 import FormWrapper from "../../components/FormWrapper";
 import Button from "../../components/Button";
 import { Text } from "react-native";
+import Store from "../../store";
 
 class Home extends Component {
   static navigationOptions = {
@@ -18,6 +19,8 @@ class Home extends Component {
       <ScreenWrapper>
         <FormWrapper>
           <Text>Home screen</Text>
+          <Text>{Store.user.name}</Text>
+          <Text>{Store.user.photoUrl}</Text>
           <Button text="Logout" onPress={this.logout} />
         </FormWrapper>
       </ScreenWrapper>
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -6,13 +6,63 @@ import FormTitle from "../../components/FormTitle";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { Text } from "react-native";
+import Store from "../../store";
+import isValidEmail from "../../utils/isValidEmail";
+import { observer } from "mobx-react";
+import { observable } from "mobx";
 
+@observer
 class Login extends Component {
   static navigationOptions = {
     header: null
   };
 
-  login = () => {
+  @observable email = "";
+  @observable password = "";
+  @observable errorMessage = "";
+  @observable isLoading = false;
+
+  onEmailChange = value => {
+    this.resetErrorMessage();
+    this.email = value;
+  };
+
+  onPasswordChange = value => {
+    this.resetErrorMessage();
+    this.password = value;
+  };
+
+  resetErrorMessage() {
+    this.errorMessage = "";
+  }
+
+  validateForm() {
+    if (!isValidEmail(this.email)) {
+      this.errorMessage = "Invalid email";
+    } else if (this.password.length < 4) {
+      this.errorMessage = "Enter password";
+    }
+  }
+
+  login = async () => {
+    this.validateForm();
+    if (this.errorMessage) {
+      return;
+    }
+
+    this.isLoading = true;
+    const response = await Store.login({
+      email: this.email,
+      password: this.password
+    });
+    this.isLoading = false;
+
+    if (response.error) {
+      this.password = "";
+      this.errorMessage = response.error.message;
+      return;
+    }
+
     this.props.navigation.navigate("HomeStack");
   };
 
@@ -22,11 +72,24 @@ class Login extends Component {
         <FormWrapper>
           <FormTitle>Log in</FormTitle>
           <FormField>
-            <Input />
+            <Input
+              textContentType={"emailAddress"}
+              keyboardType={"email-address"}
+              onChangeText={this.onEmailChange}
+            />
           </FormField>
           <FormField>
-            <Input />
+            <Input
+              textContentType={"password"}
+              secureTextEntry={true}
+              onChangeText={this.onPasswordChange}
+            />
           </FormField>
+          {!!this.errorMessage && (
+            <FormField>
+              <Text>{this.errorMessage}</Text>
+            </FormField>
+          )}
           <FormField>
             <Button text="Login" onPress={this.login} />
           </FormField>
